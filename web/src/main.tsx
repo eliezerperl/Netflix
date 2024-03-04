e04290abc8ad6602472975906600cb71ba223c35
Eliezer Perl <eliezerrules@gmail.com>
2024-03-04 10:22:35 +0200

    dotenv addition

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { axios } from '@/utils/imports.ts';
+import dotenv from "dotenv";
 
+dotenv.config()
 axios.defaults.baseURL = process.env.BASE_URL;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
