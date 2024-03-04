77bf3d7635ba0e7613bf2df263386c6c98df9310
Eliezer Perl <eliezerrules@gmail.com>
2024-03-04 10:55:36 +0200

    .

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,8 +4,12 @@ import App from './App.tsx';
 import './index.css';
 import { axios } from '@/utils/imports.ts';
 
-
-axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+axios.defaults.baseURL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8080'
+    : 'https://elisnetflixapi.netlify.app/.netlify/functions';
+console.log(axios.defaults.baseURL);
+console.log(process.env.NODE_ENV);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
