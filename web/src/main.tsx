d9b8fc9bedeef6b4a75f07e4f63cac37cdaaf65c
Eliezer Perl <eliezerrules@gmail.com>
2024-03-04 10:43:06 +0200

    .

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { axios } from '@/utils/imports.ts';
-import dotenv from "dotenv";
 
-dotenv.config()
-axios.defaults.baseURL = process.env.BASE_URL;
+
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
