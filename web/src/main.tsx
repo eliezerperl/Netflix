875b0f4ce0897d131fe563a4557560062d4d3e9b
Eliezer Perl <eliezerrules@gmail.com>
2024-03-07 11:00:53 +0200

    change to main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -6,7 +6,10 @@ import { axios } from '@/utils/imports.ts';
 
 axios.defaults.baseURL = import.meta.env.DEV
   ? 'http://localhost:8080'
-  : 'https://elisnetflixapi.netlify.app/.netlify/functions';
+  : // : 'https://elisnetflixapi.netlify.app/.netlify/functions';
+    'https://netflix-6857.vercel.app';
+console.log('dev ' + import.meta.env.DEV);
+console.log('prod ' + import.meta.env.PROD);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
