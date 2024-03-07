eae9579071bbadfb1525fd25139cde9863f396cb
Eliezer Perl <eliezerrules@gmail.com>
2024-03-07 11:18:25 +0200

    .

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://netflix-sigma-one.vercel.app/'],
+  origin: ['http://localhost:5173', 'https://netflix-sigma-one.vercel.app'],
 };
 
 app.use(cors(corsOptions));
