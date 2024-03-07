a36a816682d08f4a899d34c3ff4aee88c114407d
Eliezer Perl <eliezerrules@gmail.com>
2024-03-07 14:04:18 +0200

    url fixes

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -6,10 +6,12 @@ import { axios } from '@/utils/imports.ts';
 
 axios.defaults.baseURL = import.meta.env.DEV
   ? 'http://localhost:8080'
-  : // : 'https://elisnetflixapi.netlify.app/.netlify/functions';
-    'https://netflix-6857.vercel.app';
+  : import.meta.env.PLATFORM === 'vercel'
+  ? 'https://netflix-6857.vercel.app'
+  : 'https://elisnetflixapi.netlify.app/.netlify/functions';
 console.log('dev ' + import.meta.env.DEV);
 console.log('prod ' + import.meta.env.PROD);
+console.log('platform ' + import.meta.env.VITE_PLATFORM);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
