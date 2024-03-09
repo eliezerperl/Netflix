37323fa4f705235b8de0801bb1e6769a1f4b32d2
Eliezer Perl <eliezerrules@gmail.com>
2024-03-09 13:07:47 +0200

    browse content page

--- web/src/pages/browse/content/BrowseContentPage.tsx
+++ web/src/pages/browse/content/BrowseContentPage.tsx
@@ -1,14 +1,43 @@
-import { useParams } from '@/utils/imports';
+import { toast, useEffect, useParams, useState } from '@/utils/imports';
 import Title from '@/utils/components/shared/Title';
-import BrowseHero from '../components/BrowseHero';
+import ContentPlayer from '@/utils/components/shared/ContentPlayer';
+import { AxiosError, CustomError, getError, requestContent } from '@/lib/utils';
+import { useStoreContext } from '@/utils/context/StoreContext';
+import { Content } from '@/models/content';
 
 const BrowseContentPage = () => {
   const { title } = useParams();
+  const [content, setContent] = useState<Content>();
+  const { state } = useStoreContext();
+  const { userInfo } = state;
+
+  useEffect(() => {
+    const getContent = async () => {
+      try {
+        const data = await requestContent(userInfo, title);
+        if (data) {
+          const content: Content = data[0];
+          setContent(content);
+        }
+      } catch (error) {
+        const axiosError = error as AxiosError<CustomError>;
+        toast.error(getError(axiosError));
+      }
+    };
+    getContent();
+  }, [title, userInfo]);
 
   return (
     <>
       {title && <Title title={title} />}
-      <BrowseHero contentTitle={title} withoutActionBtns />
+      {content && (
+        <ContentPlayer
+          contentURL={content.trailer}
+          hovered
+          withoutOverlay
+          withControls
+        />
+      )}
     </>
   );
 };
