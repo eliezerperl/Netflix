2a6210167ed062df5cb8ef2ae5c8af0b3f6e69a8
Eliezer Perl <eliezerrules@gmail.com>
2024-03-11 09:09:06 +0200

    .

--- web/src/utils/components/shared/ContentPlayer.tsx
+++ web/src/utils/components/shared/ContentPlayer.tsx
@@ -4,24 +4,36 @@ type Props = {
   contentURL: string;
   hovered?: boolean | null;
   show?: (ready: boolean) => void;
+  withoutOverlay?: boolean;
+  withControls?: boolean;
 };
 
-const ContentPlayer = ({ contentURL, hovered, show }: Props) => {
+const ContentPlayer = ({
+  contentURL,
+  hovered,
+  show,
+  // withoutOverlay,
+  withControls,
+}: Props) => {
   return (
     <>
       {hovered && (
         <>
-          {/* div to not be able to interact with player */}
-          <div className="absolute top-0 left-0 w-full h-full bg-transparent z-50" />
+          {/* div to not be able to interact with player 
+          {!withoutOverlay && (
+            <div className="absolute top-0 left-0 w-full h-full bg-transparent z-50" />
+          )} */}
+
           <ReactPlayer
             url={`${contentURL}`}
+            controls={withControls}
             muted
             loop
             width={'100%'}
             height={'100%'}
             onStart={() => show && show(true)}
             playing={hovered}
-          />
+            />
         </>
       )}
     </>
