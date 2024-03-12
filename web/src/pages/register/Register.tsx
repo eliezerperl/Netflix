c2ceccb2aa14d7e187192bba1ea2cc96a7268060
Eliezer Perl <eliezerrules@gmail.com>
2024-03-12 08:42:45 +0200

    title fix

--- web/src/pages/register/Register.tsx
+++ web/src/pages/register/Register.tsx
@@ -1,11 +1,9 @@
 import RegisterContainer from '@/pages/register/RegisterContainer';
 import AuthLayout from '@/utils/components/shared/AuthLayout';
-import Title from '@/utils/components/shared/Title';
 
 const Register = () => {
   return (
     <>
-      <Title title="Welcome" />
       <AuthLayout className="bg-hero">
         <RegisterContainer />
       </AuthLayout>
