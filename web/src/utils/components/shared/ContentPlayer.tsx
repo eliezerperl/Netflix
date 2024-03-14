eaf303abeeb8fddd0633ff22afd7ba299f56859d
Eliezer Perl <eliezerrules@gmail.com>
2024-03-14 15:03:39 +0200

    unmute to contentplayer

--- web/src/utils/components/shared/ContentPlayer.tsx
+++ web/src/utils/components/shared/ContentPlayer.tsx
@@ -16,7 +16,6 @@ const ContentPlayer = ({ contentURL, hovered, show, withControls }: Props) => {
           <ReactPlayer
             url={`${contentURL}`}
             controls={withControls}
-            muted
             loop
             width={'100%'}
             height={'100%'}
