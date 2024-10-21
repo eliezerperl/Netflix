f420331018720e0588e0790dda7b11bcacbde6a9
Eliezer Perl <eliezerrules@gmail.com>
2024-10-21 16:20:21 +0300

    add analytics

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,10 +3,13 @@ import 'react-toastify/dist/ReactToastify.css';
 import { StoreProvider } from './utils/context/StoreContext';
 import { HelmetProvider } from 'react-helmet-async';
 import CustomRoutes from './utils/CustomRoutes';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <>
+      <Analytics />
+
       <HelmetProvider>
         <StoreProvider>
           <ToastContainer position="top-right" />
